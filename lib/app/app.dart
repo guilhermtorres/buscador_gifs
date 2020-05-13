91dd34f8c65db01c659b3781609524b9a9f8afab
Guilherme Torres <jguilherme.torres@gmail.com>
2020-05-12 23:46:30 -0300

    Estrutura inicial do projeto!

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:buscador_gifs/app/src/views/home_views.dart';
+import 'package:flutter/material.dart';
+
+class BuscadorGifsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
